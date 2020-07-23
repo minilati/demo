9f3bba59d13fff1792ef6433ba7325c9c75a43f8
minilati <minilati@163.com>
2020-07-23 22:57:14 +0800

    proxy

--- src/test/java/com/tk/learn/JavaTest.java
+++ src/test/java/com/tk/learn/JavaTest.java
@@ -3,11 +3,15 @@ package com.tk.learn;
 import org.junit.Test;
 import java.io.*;
 import java.sql.Driver;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class JavaTest{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
-
+        DataInputStream dis = new DataInputStream(new FileInputStream("d://"));
+        Map<String, String> stringStringHashMap = new HashMap<>();
+        Integer.valueOf(124);
     }
 }
