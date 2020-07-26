f4d56621f28f507ad6335a0429f3bf35f3cca468
minilati <minilati@163.com>
2020-07-26 13:53:37 +0800

    design

--- src/test/java/com/tk/learn/JavaTest.java
+++ src/test/java/com/tk/learn/JavaTest.java
@@ -1,17 +1,29 @@
 package com.tk.learn;
 
 import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
 import java.io.*;
 import java.sql.Driver;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class JavaTest{
     public static void main(String[] args) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        DataInputStream dis = new DataInputStream(new FileInputStream("d://"));
-        Map<String, String> stringStringHashMap = new HashMap<>();
-        Integer.valueOf(124);
+
+
+        SpelExpressionParser ss = new SpelExpressionParser();
+        Expression e = ss.parseExpression("1+2*3");
+        Object value = e.getValue();
+        System.out.println(value);
+        Integer[] aa = {2, 9, 4, 7, 1, 3, 6};
+        Arrays.sort( aa,new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
+        System.out.println(Arrays.toString(aa));
     }
 }
